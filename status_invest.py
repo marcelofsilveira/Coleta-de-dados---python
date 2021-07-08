b86dd02ba091e67577d33f1c648ed04026bddc0a
Marcelo Silveira <silveira.marcelo2010@gmail.com>
2021-07-08 15:06:43 -0300

    commit - correção "fechando o anúncio"

--- status_invest.py
+++ status_invest.py
@@ -10,17 +10,18 @@ sleep(1)
 
 print('Buscando Elemento')
 button_buscar = driver.find_element_by_xpath(
-  '//div/button[contains(@class, "find")]')
+    '//div/button[contains(@class, "find")]')
 
 button_buscar.click()
 sleep(2)
 
-print('Fechando anuncio')
-button_fechar = driver.find_element_by_class_name('btn-closetar')
-button_fechar.click()
-sleep(1)
+#print('Fechando anuncio')
+#button_fechar = driver.find_element_by_class_name('btn-close')
+# button_fechar.click()
+# sleep(1)
 
-print ('Baixando dados...')
-button_downloaded = driver.find_element_by_xpath('//div/a[contains(@class, "btn-download")]')
+print('Baixando dados...')
+button_download = driver.find_element_by_xpath(
+    '//div/a[contains(@class, "btn-download")]')
 
 button_download.click()
