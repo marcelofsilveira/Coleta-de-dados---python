ec7e6ada92fe064e0ef7fd2ef832475ab662b684
Marcelo Silveira <silveira.marcelo2010@gmail.com>
2021-07-08 12:08:55 -0300

    commit final

--- status_invest.py
+++ status_invest.py
@@ -3,18 +3,19 @@ from time import sleep
 
 URL = 'https://statusinvest.com.br/acoes/busca-avancada'
 
-print('Fazendo requisição')
+print('Fazendo requisicao')
 driver = webdriver.Chrome()
 driver.get(URL)
 sleep(1)
 
 print('Buscando Elemento')
-button _buscar = driver.find_element_by_xpath('//div/button[contains(@class, "find")]')
+button_buscar = driver.find_element_by_xpath(
+  '//div/button[contains(@class, "find")]')
 
 button_buscar.click()
 sleep(2)
 
-print('Fechando anúncio')
+print('Fechando anuncio')
 button_fechar = driver.find_element_by_class_name('btn-closetar')
 button_fechar.click()
 sleep(1)
